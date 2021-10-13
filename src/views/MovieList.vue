6fc4a8c6da95e7b4ce1e3bb4d6d74124f336c34b
David Pessanha <davidppessanha@gmail.com>
2021-10-13 20:57:17 -0300

    Created tabs menu with movie genres

--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -1,9 +1,55 @@
 <template>
-  <div class="movie-list">
-    <h1>Movie-list {{ $store.state.userId }}</h1>
+  <div class="movie-list my-10">
+    <v-card>
+      <v-toolbar flat color="yellow darken-4" dark>
+        <v-toolbar-title
+          >{{ $store.state.username.split(" ")[0] }}'s
+          movieboard</v-toolbar-title
+        >
+      </v-toolbar>
+      <v-tabs
+        background-color="yellow darken-4"
+        dark
+        color="white"
+        next-icon="mdi-arrow-right-bold-box-outline"
+        prev-icon="mdi-arrow-left-bold-box-outline"
+        show-arrows
+      >
+        <v-tab v-for="genre in movieGenres" :key="genre.name">
+          {{ genre.name }}
+        </v-tab>
+        <v-tab-item v-for="genre in movieGenres" :key="genre.name">
+          <v-card flat>
+            <v-card-text>
+              <p>
+                {{ genre.name }}
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      movieGenres: null,
+    }
+  },
+  created() {
+    this.fetchMovieGenres()
+  },
+  methods: {
+    async fetchMovieGenres() {
+      const genres = await this.$apollo.query({
+        query: require("@/graphql/getMovieGenres.gql"),
+      })
+      genres.data.genres.unshift({ name: "Recommended" })
+      this.movieGenres = genres.data.genres.slice(0, -1)
+    },
+  },
+}
 </script>
