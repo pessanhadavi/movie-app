314a901efd238d5b99ef27fbc721a5282db33042
David Pessanha <davidppessanha@gmail.com>
2021-10-13 15:57:40 -0300

    Created movie-list view

---
+++ src/views/MovieList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="movie-list"><h1>Movie-list</h1></div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
