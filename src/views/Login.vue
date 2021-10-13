cd87ae9b6c7a1b4d6af1081b2e067ad58cf03436
David Pessanha <davidppessanha@gmail.com>
2021-10-13 15:52:51 -0300

    Created login view + login route

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card width="400" class="mx-auto mt-16 pa-10 pb-5">
+    <v-card-title>
+      <h1 class="display-1">Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-text-field
+        label="Id"
+        prepend-icon="mdi-account-circle"
+        v-model.number="userId"
+      />
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions class="d-flex justify-center">
+      <v-btn color="info" class="mt-4" @click="login">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: null,
+    }
+  },
+  methods: {
+    login() {
+      if (this.userId) {
+        this.$router.push({ name: "movie-list", params: { id: this.userId } })
+      }
+    },
+  },
+}
+</script>
