d1a284f0c1be4311f566e0818bcf913eb0c8dfac
David Pessanha <davidppessanha@gmail.com>
2021-10-14 11:58:09 -0300

    Created moviecard component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="movie-card-wrapper d-flex align-center pa-3 ma-2">
+    <div class="movie-card-img">
+      <img :src="movie.poster" />
+    </div>
+
+    <div class="movie-card-text ml-10">
+      <h2 class="mb-3">{{ movie.title }}</h2>
+      <p>{{ movie.year }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    movie: Object,
+  },
+}
+</script>
+<style lang="scss" scoped>
+.movie-card-wrapper {
+  position: relative;
+  height: 210px;
+  width: 360px;
+  &:hover {
+    box-shadow: 0 0 8px rgba(238, 255, 0, 0.747);
+  }
+}
+.movie-card-img {
+  height: 190px;
+
+  img {
+    height: 100%;
+  }
+}
+h2,
+p {
+  color: black;
+}
+</style>
