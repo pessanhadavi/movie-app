dc5fee9af7ec545bf01c1b8007fd5f90e94fdb1c
David Pessanha <davidppessanha@gmail.com>
2021-10-13 23:11:15 -0300

    Put searchbar

--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="movie-list my-10">
     <v-card>
-      <v-toolbar flat color="yellow darken-4" dark>
-        <v-toolbar-title
-          >{{ $store.state.username.split(" ")[0] }}'s
-          movieboard</v-toolbar-title
-        >
+      <v-toolbar flat color="yellow darken-4" dark height="80px">
+        <v-row class="">
+          <v-col lg="7" md="12" class="pa-0 ml-3">
+            <v-text-field
+              label="Search for movie but Donnie Darko, pls"
+              color="white"
+              v-model="movieSearch"
+              :append-outer-icon="'mdi-magnify'"
+              :append-icon="movieSearch ? 'mdi-close' : ''"
+              class="pt-3"
+            />
+          </v-col>
+        </v-row>
       </v-toolbar>
       <v-tabs
         background-color="yellow darken-4"
@@ -14,6 +22,7 @@
         next-icon="mdi-arrow-right-bold-box-outline"
         prev-icon="mdi-arrow-left-bold-box-outline"
         show-arrows
+        class="mt-0"
       >
         <v-tab v-for="genre in movieGenres" :key="genre.name">
           {{ genre.name }}
@@ -37,6 +46,7 @@ export default {
   data() {
     return {
       movieGenres: null,
+      movieSearch: "",
     }
   },
   created() {
@@ -57,8 +67,6 @@ export default {
     //       genre:
     //     }
     //   })
-    //   genres.data.genres.unshift({ name: "Recommended" })
-    //   this.movieGenres = genres.data.genres.slice(0, -1)
     // },
   },
 }
